3791d6f96929e2ffae918bfcae42883d0a7bf4e8
Christian Santoni <csantoni@keybiz.it>
2024-12-11 18:08:29 +0100

    Added processing for list items

--- src/report_pipeline/pdf_processor.py
+++ src/report_pipeline/pdf_processor.py
@@ -2,7 +2,7 @@ from enum import Enum
 from llmsherpa.readers import LayoutPDFReader, Block, Paragraph, Section, Table, ListItem, Document
 from chromadb import Client, Settings
 from langchain_community.embeddings import OpenAIEmbeddings
-
+from dataclasses import dataclass, field
 
 import json
 import os
@@ -17,14 +17,16 @@ class BlockType(Enum):
 class ParsedDocument:
     pass
 
-class ContentBlock(NamedTuple):
-    block_idx: int
-    bbox: tuple
-    original_text: str
-    processed_text: str
-    page_idx: int
-    parent_chain: list[str]
-    block_type: BlockType
+@dataclass
+class ContentBlock:
+    block_idx: int = 0
+    bbox: tuple = (0,0,0,0)
+    original_text: str = ""
+    processed_text: str = ""
+    page_idx: int = 0
+    parent_chain: list[str] = field(default_factory=list)
+    block_type: BlockType = BlockType.PARAGRAPH
+    
 
 class PDFProcessor:
     def __init__(self):
@@ -98,8 +100,56 @@ class PDFProcessor:
         return " > ".join(ancestors)
     
     def _process_list_items(self):
-        # aggregate by id
-        return []
+        self.list_items.sort(key=lambda x: x.block_idx)
+        
+        current_list : List[ListItem] = []
+        processed_lists : List[ContentBlock] = []
+        
+        for item in self.list_items:
+            if not current_list:
+                current_list.append(item)
+            else:
+                prev_idx = current_list[-1].block_idx
+                curr_idx = item.block_idx
+                
+                if curr_idx == prev_idx + 1:
+                    current_list.append(item)
+                else:
+                    processed_lists.append(self._aggregate_list_items(current_list))
+                    current_list = [item]
+        
+        if current_list:
+            processed_lists.append(self._aggregate_list_items(current_list))
+        
+        for p_l in processed_lists:
+            self.content_blocks.append(p_l)
+        
+        return processed_lists
+        
+    def _aggregate_list_items(self, items: List[ListItem]) -> ContentBlock:
+        """Combine list items into a single entry. Add context"""
+        
+        block = ContentBlock()
+        block.block_idx = min([item.block_idx for item in items])
+        block.bbox = (
+            min([item.bbox[0] for item in items]),
+            min([item.bbox[1] for item in items]),
+            max([item.bbox[2] for item in items]),
+            max([item.bbox[3] for item in items])
+        )
+        block.block_type = BlockType.LIST
+        block.page_idx = min([item.page_idx for item in items])
+        block.parent_chain = self._format_parent_chain(items[0])
+        
+        text = ""
+        for item in items:
+            text += " ".join(item.sentences)
+            text += "\n"
+        
+        block.original_text = text
+        block.processed_text = self._sanitize_text(text)
+        
+        return block
         
     def _process_tables(self):
         # to_text -> markdown + context (parent_chain)
@@ -114,10 +164,9 @@ if __name__ == "__main__":
     content_blocks = pdf_processor.extract_text("./data/2023-annual-report-short.pdf")
     
     pdf_processor.document_stats()
-    
-    for t in pdf_processor.tables:
-        print(t.to_context_text())
-        print("=" * 80)
-        print(t.to_html())
-        print("Markdown")
-        print(md(t.to_html()))
+
+    for block in pdf_processor.content_blocks:
+        if block.block_type == BlockType.LIST:
+            print(block.parent_chain)
+            print(block.processed_text)
+            print()
